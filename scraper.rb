2d2024e485e90ca7bf58b262ac3130c86120db46
Eric Tam <eric.tam@traceyanderic.com>
2017-05-23 11:43:58 +1000

    added additional console output

--- scraper.rb
+++ scraper.rb
@@ -63,8 +63,9 @@ end
 
 applications.each do |record|
   if (ScraperWiki.select("* from data where `council_reference`='#{record[:council_reference]}'").empty? rescue true)
+    puts "Saving record " + record[:council_reference] + " - " + record[:address]
     ScraperWiki.save_sqlite([:council_reference], record)
   else
-     puts "Skipping already saved record " + record[:council_reference]
+    puts "Skipping already saved record " + record[:council_reference]
   end
 end
